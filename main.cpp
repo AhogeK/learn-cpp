da6b7a5dca4b948f0917635ddfd1b599068e584a
AhogeK <ahogek@gmail.com>
2021-09-24 14:03:56 +0800

    feat(chapter1): exercise 1-1

--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include "part_1/listing_1.cuh"
-int main()
-{
+#include "part_1/exercise_1.cuh"
+
+int main() {
     std::cout << "Hello, World!" << std::endl;
-    listing_1_7();
+    exercise_1_1();
     return 0;
 }
---
+++ part_1/exercise_1.cuh
@@ -0,0 +1,10 @@
+//
+// Created by ahogek on 9/24/21.
+//
+
+#ifndef LEARN_CPP_EXERCISE_1_CUH
+#define LEARN_CPP_EXERCISE_1_CUH
+
+void exercise_1_1();
+
+#endif //LEARN_CPP_EXERCISE_1_CUH
