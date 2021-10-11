8f19e0fa1fb4f1b3b1741262bf705c7383e6b0ac
AhogeK <ahogek@gmail.com>
2021-10-11 19:31:01 +0800

    feat(Part I Chapter 2): listing 2-20
    Listing 2-20: An updated ClockOfTheLongNow from Listing 2-19 that encapsulates year

--- part_1/listing_2.cuh
+++ part_1/listing_2.cuh
@@ -32,6 +32,41 @@ struct ClockOfTheLongNow {
     int year;
 };
 
+/**
+ * Listing 2-20: An updated ClockOfTheLongNow from Listing 2-19 that encapsulates year
+ */
+struct ClockOfTheLongNow2 {
+    void add_year() {
+        year++;
+    }
+    bool set_year(int new_year) {
+        if (new_year < 2019) return false;
+        year = new_year;
+        return true;
+    }
+    int get_year() {
+        return year;
+    }
+private:
+    int year;
+};
+
+class ClockOfTheLongNowClass {
+    int year;
+public:
+    void add_year() {
+        year++;
+    }
+    bool set_year(int new_year) {
+        if (new_year < 2019) return false;
+        year = new_year;
+        return true;
+    }
+    int get_year() {
+        return year;
+    }
+};
+
 union Variant {
     char string[10];
     int integer;
